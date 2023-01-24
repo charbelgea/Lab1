8af0bfaf56afe9b17d0adf6a1c71fc9fab28eafe
charbel geagea <charbelgeagea112@gmail.com>
2023-01-24 21:14:08 +0200

    final

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me{
+ public static void main(String[] args){
+   System.out.println("charbel geagea");
+    System.out.println("18");
+     System.out.println("computer engineering");
+ }
+}
