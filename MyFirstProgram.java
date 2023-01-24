9ce3ab96bebec651732b5f892bd5803a97ecd7b9
charbel geagea <charbelgeagea112@gmail.com>
2023-01-24 19:20:37 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram
+{
+    public static void main(String[] args){
+          System.out.println("this is my");
+          System.out.println("first Java program");
+          System.out.println("programing is fun!");
+    }
+}
